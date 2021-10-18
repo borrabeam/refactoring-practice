dfa289d791d6e5313a17fae1b4ee352fd3071e8d
borrabeam <bannakorn.s@ku.th>
2021-10-18 15:02:35 +0700

    Refactor gamelib.py

--- game_framework/gamelib.py
+++ game_framework/gamelib.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+CANVAS_WIDTH = 800
+CANVAS_HEIGHT = 500
+TIMER_DELAY = 33 # milliseconds
+
 class GameCanvasElement():
     """Base class for an element on the game canvas, with attributes:
 
@@ -36,13 +40,17 @@ class GameCanvasElement():
         if self.is_visible:
             self.canvas.coords(self.canvas_object_id, self.x, self.y)
 
+    # Hook method
     def init_canvas_object(self):
+        """ A method that a subclass can override to initialize itself"""
         pass
 
+    # Hook
     def init_element(self):
         pass
 
     def update(self):
+        """Update the object to emulate animation. """
         pass
 
 
@@ -83,7 +91,10 @@ class GameApp(ttk.Frame):
     on the canvas, start/stop animation, and running the animation loop.
     """
     
-    def __init__(self, parent, canvas_width=800, canvas_height=500, update_delay=33):
+    def __init__(self, parent, 
+                 canvas_width=CANVAS_WIDTH, 
+                 canvas_height=CANVAS_HEIGHT, 
+                 update_delay=TIMER_DELAY):
         super().__init__(parent)
         self.parent = parent
         
@@ -92,8 +103,8 @@ class GameApp(ttk.Frame):
         
         self.update_delay = update_delay
 
-        self.grid(sticky="news")
-        self.create_canvas()
+        self.grid(sticky=tk.NSEW)
+        self.canvas = self.create_canvas(canvas_width,canvas_height)
 
         self.elements = []
         self.init_game()
@@ -101,12 +112,13 @@ class GameApp(ttk.Frame):
         self.parent.bind('<KeyPress>', self.on_key_pressed)
         self.parent.bind('<KeyRelease>', self.on_key_released)
         
-    #TODO refactor this - don't depend on side effects
-    def create_canvas(self):
-        self.canvas = tk.Canvas(self, borderwidth=0,
-            width=self.canvas_width, height=self.canvas_height, 
+    #efactor this - don't depend on side effects
+    def create_canvas(self,width,height):
+        canvas = tk.Canvas(self, borderwidth=0,
+            width=width, height= height, 
             highlightthickness=0)
-        self.canvas.grid(sticky="news")
+        canvas.grid(sticky=tk.NSEW)
+        return canvas
 
     def animate(self):
         self.pre_update()
@@ -121,6 +133,16 @@ class GameApp(ttk.Frame):
 
     def start(self):
         self.after(0, self.animate)
+
+    def add_element(self, element: GameElement):
+        """Add an element to the game."""
+        self.elements.append(element)
+
+    def remove_element(self, element: GameElement):
+        """Remove an element from the game"""
+        if element in self.elements:
+            self.elements.remove(element)
+            self.canvas.delete(element.canvas_object_id)
 
     def init_game(self):
         pass
